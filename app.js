13a31638d2948e5706d145a3230f2740f5cf4c28
Davi Dias <davitdt206@gmail.com>
2024-04-18 22:07:55 -0300

    update

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-
+// Selecionar elementos
 const hamburger = document.querySelector('.hamburger');
-const navList = document.querySelector('.nav');
+const nav = document.querySelector('.nav');
+const navList = nav.querySelector('.nav-list'); // Atualização: encontrar .nav-list dentro de .nav
+const navLinks = navList.querySelectorAll('a'); // Atualização: selecionar todos os links dentro de .nav-list
 
-hamburger.addEventListener('click', () => 
-    navList.classList.toggle('active')
-);
+// Adicionar evento de clique ao botão hamburger
+hamburger.addEventListener('click', () => {
+    // Alternar a classe 'active' em nav
+    nav.classList.toggle('active');
+});
+
+// Adicionar evento de clique a cada link na lista de navegação
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        // Remover a classe 'active' de nav para fechar o menu
+        nav.classList.remove('active');
+    });
+});
